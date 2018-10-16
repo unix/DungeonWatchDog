16c9f8b01ded96af471ada3d7fed6e11fd0a1a8b
WittBulter <nanazuimeng123@gmail.com>
2018-10-17 00:53:31 +0800

    build: v2.2.1

--- constants.lua
+++ constants.lua
@@ -3,7 +3,7 @@ local Constants = addon:NewModule('Constants')
 
 local infos = {
     ADDON_BASE_NAME = 'DungeonWatchDog',
-    VERSION = '2.2.0',
+    VERSION = '2.2.1',
     EXPORT_PLANEL_FRAME = 'exportPanelFrame',
     KEYWORD_PLANEL_FRAME = 'keywordPanelFrame',
     DEFAULT_EXPORT_SEP = '0Z0',
